1f8bff642789cb00f1514e11bed4f21a96993da7
kevenvz <510966@student.glu.nl>
2016-04-21 11:50:59 +0200

    Removed unnecessary files

--- src/main/java/com/github/kevenvz/timetable/social/UriQueryBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.github.kevenvz.timetable.social;
-
-import org.springframework.social.google.api.query.QueryBuilder;
-
-import java.net.URI;
-
-public interface UriQueryBuilder<Q extends UriQueryBuilder<?, T>, T> extends QueryBuilder<Q, T> {
-    URI buildUri();
-}
