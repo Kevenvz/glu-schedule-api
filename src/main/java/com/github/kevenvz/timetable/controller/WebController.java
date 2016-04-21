b50e374d8344e145c283f870c993036bf1becfca
kevenvz <510966@student.glu.nl>
2016-04-21 11:51:32 +0200

    Event listing at home

--- src/main/java/com/github/kevenvz/timetable/controller/WebController.java
+++ src/main/java/com/github/kevenvz/timetable/controller/WebController.java
@@ -1,6 +1,7 @@
 package com.github.kevenvz.timetable.controller;
 
 import com.github.kevenvz.timetable.social.Google;
+import com.github.kevenvz.timetable.social.calendar.Calendar;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -26,9 +29,19 @@ public class WebController {
         if (connectionRepository.findPrimaryConnection(Google.class) == null) {
             return "redirect:/connect/google";
         }
+
+        List<Calendar> calendars = google.calendarOperations().getAllCalendars().getCalendars();
+        Optional<Calendar> roosterCalendar = calendars.stream()
+                .filter(calendar -> calendar.getSummary().equals("Rooster"))
+                .findFirst();
+
         model.addAttribute("accessToken", google.getAccessToken());
         model.addAttribute("name", google.plusOperations().getGoogleProfile().getDisplayName());
-        model.addAttribute("calendars", google.calendarOperations().getAllCalendars().getCalendars());
+        model.addAttribute("calendars", calendars);
+
+        if (roosterCalendar.isPresent())
+            model.addAttribute("events", google.calendarOperations().getEventList(roosterCalendar.get().getId()).getEvents());
+
         return "home";
     }
 }
